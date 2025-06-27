0dea0318fc54c0f6335cc86d863900003612d150
Clinton Jacos <clintonogembo70@gmail.com>
2025-06-27 14:26:18 +0300

    replaced single quotes with double quotes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get '/current_time', to: 'time#current_time'
-  
+  get "/current_time", to: "time#current_time"
+
 end
