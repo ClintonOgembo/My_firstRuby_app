fce2ff29d0efbd898f01f9dad437cbaae72d9791
Clinton Jacos <clintonogembo70@gmail.com>
2025-07-03 13:49:14 +0300

    modified currency_convertions

--- app/controllers/currency_convertions_controller.rb
+++ app/controllers/currency_convertions_controller.rb
@@ -1,11 +1,14 @@
 class CurrencyConvertionsController < ApplicationController
     def create
+        amount = params[:amount].to_f
+
         render(
             json:{
-                tsh: 100,
-                ugx: 10,
-                usd: 88,
-            }, status: :ok
+                TZS: (amount * 20.40).round(2),
+                UGX: (amount * 27.80).round(2),
+                USD: (amount * 0.00774).round(2)
+            },
+            status: :ok
         )
     end    
 end    
--- spec/requests/currency_convertions_spec.rb
+++ spec/requests/currency_convertions_spec.rb
@@ -1,30 +1,55 @@
 require "rails_helper"
 
 RSpec.describe "sample test" do
-    it "respond with 200" do
-        post "/currency_convertions"
-
-        expect(status).to eq(200)
+    it "responds with 200" do
+        post "/currency_convertions", params: { amount: 100}
+        expect(response).to have_http_status(:ok)
     end
 
-    it "respond with correct conversions" do
-        post "/currency_convertions"
-
+    it "responds with correct TZS conversions" do
+        post "/currency_convertions", params: { amount: 100}
         parsed_body = response.parsed_body
-        expect(parsed_body[:tsh]).to eq(100)
+        expect(parsed_body["TZS"]).to eq((100 * 20.40).round(2))
     end
-
-    it "respond with correct conversions" do
-        post "/currency_convertions"
-
+    
+    it "responds with correct UGX conversions" do
+        post "/currency_convertions", params: { amount: 100}
         parsed_body = response.parsed_body
-        expect(parsed_body[:ugx]).to eq(10)
+        expect(parsed_body["UGX"]).to eq((100 * 27.80).round(2))
     end
-
-    it "respond with correct conversions" do
-        post "/currency_convertions"
-
+    
+    it "responds with correct USD conversions" do
+        post "/currency_convertions", params: { amount: 100}
         parsed_body = response.parsed_body
-        expect(parsed_body[:usd]).to eq(88)
-    end
+        expect(parsed_body["USD"]).to eq((100 * 0.00774).round(2))
+    end 
+
+
+#     it "respond with 200" do
+#         post "/currency_convertions"
+
+#         expect(status).to eq(200)
+#     end
+
+#     it "respond with correct conversions" do
+#         post "/currency_convertions"
+
+#         parsed_body = response.parsed_body
+#         expect(parsed_body[:tsh]).to eq(100)
+#     end
+
+#     it "respond with correct conversions" do
+#         post "/currency_convertions"
+
+#         parsed_body = response.parsed_body
+#         expect(parsed_body[:ugx]).to eq(10)
+#     end
+
+#     it "respond with correct conversions" do
+#         post "/currency_convertions"
+
+#         parsed_body = response.parsed_body
+#         expect(parsed_body[:usd]).to eq(88)
+#     end
+
 end
