141ac8bbed5c19d4e4f8434779ed04f25c60e415
Clinton Jacos <clintonogembo70@gmail.com>
2025-07-03 11:18:26 +0300

    views

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,9 +1,14 @@
 class DemoController < ApplicationController
     def show
+        @message= "Hello from show action"
     end
+
     def update
+        render plain: "Update action called"
     end
+
     def delete
+        render plain: "Destroy action called"
     end
 
 end
