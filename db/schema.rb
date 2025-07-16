717847408e433ec88c35af3c0db29b2160fee623
Clinton Jacos <clintonogembo70@gmail.com>
2025-07-16 18:24:34 +0300

    correcting lint issue

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema[8.0].define(version: 2025_07_15_123319) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_16_152257) do
   create_table "movie_genres", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -10,5 +10,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_15_123319) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.string "password"
   end
 end
