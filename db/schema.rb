00b1fe420601fb4b423b64b85677773dd047ef24
Clinton Jacos <clintonogembo70@gmail.com>
2025-07-16 17:32:55 +0300

    corrected lint issue

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_15_123319) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.integer "age"
