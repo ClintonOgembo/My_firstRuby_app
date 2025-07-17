f193eef340d8840bc6a97f73ac85c646a111444a
Clinton Jacos <clintonogembo70@gmail.com>
2025-07-17 11:17:32 +0300

    added pk to my tables though aren't visible in the schema.rb file because its SQLite db

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_17_062433) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_17_071213) do
   create_table "movie_genres", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -26,12 +25,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_17_062433) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: false, force: :cascade do |t|
+    t.string "email", null: false
     t.string "name"
     t.integer "age"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email"
-    t.string "password"
   end
 end
